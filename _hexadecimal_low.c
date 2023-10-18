d0f36edea16f7628f6dc982f993c7d2229226dab
Yahia-Ka <yahia.karkori@gmail.com>
2023-10-18 02:04:30 +0100

    Work in progress

---
+++ _hexadecimal_low.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _hexadecimal_low - Print a number in hexadecimal format
+ * @list: Number to print
+ *
+ * Return: Length of the number
+ **/
+int _hexadecimal_low(va_list list)
+{
+	char *p_buff;
+	int size;
+
+	p_buff = itoa(va_arg(list, unsigned int), 16);
+
+	size = print((p_buff != NULL) ? p_buff : "NULL");
+
+	return (size);
+}
---
+++ _integer.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _integer - Print a number in base 10
+ * @list: Number to print in base 10
+ *
+ * Return: Length of th numbers in decimal
+ **/
+int _integer(va_list list)
+{
+	char *p_buff;
+	int size;
+
+	p_buff = itoa(va_arg(list, int), 10);
+
+	size = print((p_buff != NULL) ? p_buff : "NULL");
+
+	return (size);
+}
